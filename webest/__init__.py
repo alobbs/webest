f27ed615b36199b50acdf7e5248ee51ccc2397fa
Alvaro Lopez Ortega <alvaro@gnu.org>
2016-01-01 21:19:18 +0100

    Imports Scroll class

--- webest/__init__.py
+++ webest/__init__.py
@@ -6,4 +6,5 @@ __author__ = 'Alvaro Lopez Ortega'
 __copyright__ = 'Copyright 2015 Alvaro Lopez Ortega'
 
 from . import browser
+from . import scroll
 from .obj import *
---
+++ webest/scroll.py
@@ -0,0 +1,13 @@
+import retrying
+
+from . import exceptions as ex
+
+
+@retrying.retry(wait_fixed=1000, retry_on_exception=ex.is_retry_exception)
+def go_bottom(b):
+    b.execute_script("window.scrollBy(0,100000)")
+
+
+@retrying.retry(wait_fixed=1000, retry_on_exception=ex.is_retry_exception)
+def go_top(b):
+    b.execute_script("window.scrollBy(0,0)")
