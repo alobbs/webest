c32f3ea6b885517dd5d0690fad9c162c243d982f
Alvaro Lopez Ortega <alvaro@gnu.org>
2017-04-20 21:31:45 -0400

    Adds a focus() function

--- webest/dom.py
+++ webest/dom.py
@@ -1,3 +1,10 @@
+import obj
+from selenium.webdriver.common.keys import Keys
+
 def remove_children(b, selector):
     js = """document.querySelector('%s').innerHTML = '';""" % (selector)
     b.execute_script(js)
+
+def focus(b, selector):
+    o = obj.get(selector)
+    o.send_keys(Keys.NULL)
