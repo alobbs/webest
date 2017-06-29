903077eeec809879de1fb96d7f05e2bc42cdc596
Alvaro Lopez Ortega <alopezor@akamai.com>
2017-06-29 16:02:13 -0400

    Expose Selenium's keys as webest.Key

--- webest/__init__.py
+++ webest/__init__.py
@@ -5,6 +5,8 @@ __email__ = 'alvaro@alobbs.com'
 __author__ = 'Alvaro Lopez Ortega'
 __copyright__ = 'Copyright 2015 Alvaro Lopez Ortega'
 
+from selenium.webdriver.common.keys import Keys
+
 from . import browser
 from . import dom
 from . import scroll
